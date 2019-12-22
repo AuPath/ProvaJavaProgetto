bfce65d0f95798dfcedfb27e937bbcc76b9be12c
AuPath <marco.au.grassi98@protonmail.com>
2019-12-22 12:52:18 +0100

    set up pom.xml and wrote 1 unit test, ready for Travis CI integration

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,16 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+class AppTests {
+
+	@DisplayName("BOH")
+	@Test
+	void prova() {
+		assertEquals(2, 1 + 1, "prova");
+	}
+
 }
